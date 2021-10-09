b5984965c1da3f583b10bd668fcd2f3eedc79557
YiJoonYong <yijy001@gmail.com>
2021-10-09 22:14:40 +0900

    211009 blog project submit blog data and redirect

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Blog
+from django.utils import timezone
 
 def home(request):
     blog = Blog.objects #쿼리셋
@@ -8,3 +9,14 @@ def home(request):
 def detail(request, blog_id):
     details = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'detail.html', {'details' : details})
+
+def write(request):
+    return render(request, 'write.html' )
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/'+str(blog.id))
