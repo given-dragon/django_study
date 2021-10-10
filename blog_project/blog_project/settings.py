1e7fd48e42f311a8f74099a793d466b791996af6
YiJoonYong <yijy001@gmail.com>
2021-10-10 23:54:36 +0900

    211010 heroku

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-1_du^3ek2wg+l8v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
