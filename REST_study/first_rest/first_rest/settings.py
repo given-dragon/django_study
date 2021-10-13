409b80c10c7357aa1d2ef45f360ff82220cc9701
YiJoonYong <yijy001@gmail.com>
2021-10-13 14:28:26 +0900

    211013 pagination, filtering, search

--- REST_study/first_rest/first_rest/settings.py
+++ REST_study/first_rest/first_rest/settings.py
@@ -22,16 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 secret_file = os.path.join(BASE_DIR, 'secrets.json')    #json위치 표시
 with open(secret_file) as f:
     secrets = json.loads(f.read()) 
-def get_secrets(setting, secrets=secrets):
+def get_secret(setting, secrets=secrets):
     try:
         return secrets[setting]
     except KeyError:
         error_msg = "Set the {} enviroment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
-
+        
+SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
